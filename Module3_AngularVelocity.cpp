60cd55a6d0d74f596086bb6ba2d7415dfbe96633
Josh Lowe <lowe.joshua.41@gmail.com>
2019-09-29 21:31:05 -0400

    Finalize code submission

--- Module3_AngularVelocity.cpp
+++ Module3_AngularVelocity.cpp
@@ -11,39 +11,48 @@
  *
  * Uses an IR LED and IR phototransistor to implement an optical tachometer.
  * The IR LED is connected to pin 13 and ran continually. A status LED is connected
- * to pin 12. Pin 2 (interrupt 0) is connected across the IR detector.
+ * to pin 12. Pin 2 (interrupt 0) is connected across the IR detector. Motor is connected
+ * to pin 3.
  *
- *
+ * This implementation uses a mutex to control access to shared data.
+ * Whenever the interrupt routine wants to update is count, it must first check
+ * the mutex state. If the mutex is available, it will update the count and exit.
+ * The mutex is actuated by the main task loop. This processing path periodically checks
+ * the interrupt counter, makes some calculations, prints the result, and then waits.
+ * The critical section of this block is the reading of the counter. Whenever it wants
+ * to do so, it must take the mutex, preventing the interrupt for modifying it, but also
+ * omitting any interrupt events which occur during the time the mutex is taken.
  */
 
 #include <Arduino.h>
-//#include <DataStore.h>
 
 #define MOTOR_PIN   3   // Motor/fan is connected to digital pin 3
 #define IRDET_PIN   2   // IR detector output connected to digital pin 2
-#define IRLED_PIN   12
+#define IRLED_PIN   12  // IR emitter connected to digital pin 12
 
-#define MOTOR_VAL_Q 55
+#define MOTOR_VAL_Q 55  // Controls the speed of the motor. Usable range: (35,50)
 
-#define SAMPLE_MS   1000
+#define SAMPLE_MS   1000    // Interrupt sampling rate.
 
-#define MIN_TO_SEC  60
-#define SEC_TO_MSEC 1000
+#define PRINT_TIMEOUT 2     // Control print frequency.
 
-volatile unsigned intrCount = 0;
+volatile unsigned intrCount = 0;    // Number of interrtupts that have occured.
 
-bool fCountMutex = false;
+volatile bool fCountMutex = false;   // Mutex to control access to above count.
 
 unsigned long prevSampleTime = 0;
 unsigned totalRotations = 0;
 
-char printTimeout = 10;
+char printTimeout = 0;
 
 float angularVelocity = 0.0f;
 
 /*
  * Primary Interrupt Function
  * Records number of times IR Emitter->Detector link is broken.
+ * In an effort to suppress erronous interrupts, the interrupt pin is immediately
+ * read to see if the link is still down, only counting the interrupt if the return
+ * value is logic 0.
  */
 void isr0() {  if (!fCountMutex) {(!digitalRead(IRDET_PIN))? ++intrCount : 0;} }
 
@@ -57,13 +66,14 @@ void setup()
     pinMode(MOTOR_PIN, OUTPUT);
     pinMode(IRLED_PIN, OUTPUT);
 
+    // Active the LED.
     digitalWrite(IRLED_PIN, HIGH);
 
     //The IR detector is connected to a digital pin, convert to interrupt pin.
     //Triggers on FALLING (change from HIGH to LOW)
     attachInterrupt(digitalPinToInterrupt(IRDET_PIN), isr0, FALLING);
 
-    //Turn the fan on
+    //Turn the motor on
     analogWrite(MOTOR_PIN, MOTOR_VAL_Q);
 }
 
@@ -71,21 +81,22 @@ void loop()
 {
     fCountMutex = true;      // Take the mutex
 
-    totalRotations = intrCount / 2;
+    totalRotations = intrCount / 2; // Account for multiple interrupts per rotation.
 
-    intrCount = 0;
+    intrCount = 0;      // Reset the counter.
 
     fCountMutex = false;    // Return the Mutex
 
-    prevSampleTime = millis();
+    prevSampleTime = millis();  // Save last record time.
 
+    // angularVelocity would allow for modifications to the printed unit.
+    // If desired, the appropriate time quanta could be substituted for the denominator of this equation.
     angularVelocity = static_cast<float>(totalRotations)/*/(millis() - prevSampleTime)*/;
 
     //Write it out to serial port
-    if (printTimeout++ % 2 == 0)
-    {
-        Serial.print(prevSampleTime); Serial.print(','); Serial.println(totalRotations);
-    }
+    // The current implementation displays:
+    //  SAMPLE_TOA (msec) : FREQ (Hz)
+    Serial.print(prevSampleTime); Serial.print(','); Serial.println(angularVelocity);
 
     delay(SAMPLE_MS);   // Wait for next sample
 }
